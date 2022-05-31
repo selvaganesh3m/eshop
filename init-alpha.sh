7e32864a87ccba5fb01438c0c62958e840d3f3ee
selvaganesh3m <selvaganesh>
2022-05-31 18:57:10 +0530

    Categories added

--- init-alpha.sh
+++ init-alpha.sh
@@ -1,2 +1,4 @@
+python manage.py makemigrations
 python manage.py migrate
-python manage.py loaddata superuser.json
+python manage.py loaddata super_user.json
+python manage.py loaddata categories.yaml
